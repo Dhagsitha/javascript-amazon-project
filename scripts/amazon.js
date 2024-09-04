d317abbf529f5a76d66f4a4440038147367eb440
Dhagsitha <dhagsithag@gmail.com>
2024-09-04 21:25:01 +0530

    fix price formatting

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,4 @@
-const products=[
-    {
-        image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating:{
-            stars:4.5,
-            coount:87
-        },
-        priceCents:1090
-    },
-    {
-        image:'images/products/intermediate-composite-basketball.jpg',
-        name: ' Intermediate Size Basketball',
-        rating:{
-            stars:4,
-            coount:127
-        },
-        priceCents:2095
-    },
-    {
-        image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: ' Adults Plain Cotton T-Shirt - 2 Pack ',
-        rating:{
-            stars:4.5,
-            coount:56
-        },
-        priceCents:799
-    }
-    
-];
+
 let ProductsHTML='';
 
 products.forEach((product)=>{
@@ -76,7 +47,7 @@ products.forEach((product)=>{
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
                 Add to Cart
             </button>
             </div>
@@ -87,3 +58,29 @@ products.forEach((product)=>{
     //console.log(html);
     //Using Dom,we put it on the webpage
     document.querySelector('.js-products-grid').innerHTML=ProductsHTML;
+    document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+    button.addEventListener('click',()=>
+    {
+        const productId=button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item)=>{
+        if(productId===item.productId)
+        {
+    matchingItem=item;
+        }
+        })
+        if(matchingItem)//truthy value
+        {
+        matchingItem.quantity+=1;
+        }
+        else
+        {
+        cart.push({
+            productId:productId,
+            quantity:1
+        })
+    }
+    })
+
+    });
