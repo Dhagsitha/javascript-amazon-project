090854fec7765ebc8e361c4f4a769c06ffb236eb
Dhagsitha <dhagsithag@gmail.com>
2024-09-04 22:26:29 +0530

    Delete products from cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,42 +1,41 @@
-
-let ProductsHTML='';
-
-products.forEach((product)=>{
-    ProductsHTML+=`
+import { cart ,addToCart} from '../data/cart.js';
+import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
+let ProductsHTML = '';
+products.forEach((product) => {
+    ProductsHTML += `
         <div class="product-container">
             <div class="product-image-container">
-                <img class="product-image"
-                src="${product.image}">
+                <img class="product-image" src="${product.image}">
             </div>
 
             <div class="product-name limit-text-to-2-lines">
-                "${product.name}"
+                ${product.name}
             </div>
 
             <div class="product-rating-container">
-                <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars*10}.png">
+                <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
                 <div class="product-rating-count link-primary">
-                ${product.rating.count}
+                    ${product.rating.count}
                 </div>
             </div>
 
             <div class="product-price">
-                ${(product.priceCents/100).toFixed(2)}
+                $${(formatCurrency(product.priceCents))}
             </div>
 
             <div class="product-quantity-container">
                 <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
                 </select>
             </div>
 
@@ -50,37 +49,29 @@ products.forEach((product)=>{
             <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
                 Add to Cart
             </button>
-            </div>
+        </div>
+    `;
+});
 
-        `
-        
-    })
-    //console.log(html);
-    //Using Dom,we put it on the webpage
-    document.querySelector('.js-products-grid').innerHTML=ProductsHTML;
-    document.querySelectorAll('.js-add-to-cart')
-    .forEach((button)=>{
-    button.addEventListener('click',()=>
-    {
-        const productId=button.dataset.productId;
-        let matchingItem;
-        cart.forEach((item)=>{
-        if(productId===item.productId)
-        {
-    matchingItem=item;
-        }
-        })
-        if(matchingItem)//truthy value
-        {
-        matchingItem.quantity+=1;
-        }
-        else
-        {
-        cart.push({
-            productId:productId,
-            quantity:1
-        })
-    }
-    })
+// Insert the product HTML into the DOM
+document.querySelector('.js-products-grid').innerHTML = ProductsHTML;
 
+
+function updateCartQuantity()
+{
+    let initialCartQuantity = 0;
+cart.forEach((cartItem) => {
+    initialCartQuantity += cartItem.quantity;
+});
+document.querySelector('.js_cart_quantity').innerHTML = initialCartQuantity;
+
+}
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;
+            addToCart(productId);
+            updateCartQuantity();
+        });
     });
+console.log(cart);
